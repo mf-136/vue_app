9bdf33f961d38dd45fecb2107f65bf9b54288223
mf <57146292+mf-136@users.noreply.github.com@users.noreply.github.com>
2020-01-27 23:55:48 +0000

    temp

--- app/javascript/components/micropostPage.vue
+++ app/javascript/components/micropostPage.vue
@@ -7,7 +7,7 @@
             <el-timeline-item placement="top">
               <el-card>
                 <h4>{{ m.content }}</h4>
-                <p>{{ getUserName(m.user_id) }} posted {{ m.created_at }}</p>
+                <p>posted {{ m.created_at }}</p>
               </el-card>
             </el-timeline-item>
           </el-timeline>
@@ -34,13 +34,13 @@ export default {
       .then(response => (this.microposts = response.data))
   },
   methods: {
-    getUserName: function(user_id) {
-      let name = '';
-      axios
-        .get(`/api/v1/users/${user_id}.json`)
-        .then(response => (this.name = response.data.name));
-      return name;
-    }
+    // getUserName: function(user_id) {
+    //   let name = '';
+    //   axios
+    //     .get(`/api/v1/users/${user_id}.json`)
+    //     .then(response => (this.name = response.data.name));
+    //   return name;
+    // }
   }
 }
 </script>
